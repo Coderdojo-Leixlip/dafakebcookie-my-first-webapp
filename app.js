d740f336d3d917e4e46549345171ef356b560978
dafakebcookie <35631483+dafakebcookie@users.noreply.github.com>
2018-05-12 13:52:29 +0100

    work
    i did work

--- app.js
+++ app.js
@@ -3,10 +3,38 @@ const http = require ('http');
 const hostname = "127.0.0.1";
 const port = 8080
 
+const htmlhome = `
+<html> 
+
+<body> <p> hello person  </p>
+
+</body>
+
+</html>
+ `
+const page2 = `
+
+<html>
+
+<body> 
+
+<p>hello page 2</p>  
+
+</body>
+
+
+</html>
+`
 const server = http.createServer((req, res)  => {
  res.statusCode = 200;   
- res.setHeader('content-type','text/plain');  
- res.end('hello world!\n');  
+ res.setHeader('content-type','text/html');     
+if (req.url === '/'){
+    res.end (htmlhome);
+}
+else if (req.url === '/page2/') { 
+    res.end (page2); 
+}
+
 })
 
 server.listen(port, hostname, function(){
